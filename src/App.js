c0440c4ca262feb474bc7ceece1f422947906417
kanhaiya <kanhaiyarathore375gmail.com@kanhaiyas-MacBook-Air.local>
2025-09-10 04:56:24 +0530

    Updated portfolio with new changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Index from './pages/index'
-import Demo from './pages/demo'
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="/demo" element={<Demo />} />
+     
       </Routes>
     </Router>
   );
